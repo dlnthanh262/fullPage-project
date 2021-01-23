9b8305d929e1bc6e2e827e7e48b9da15ccf21f09
dolenguyetthanh <dolenguyetthanh@gmail.com>
2021-01-23 20:38:48 +0700

    Add main.js and main.css

---
+++ js/main.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true,
+	navigation: true,
+	anchors: ['home', 'aboutme', 'skills', 'target', 'contact'],
+	navigationTooltips: ['Home', 'About Me', 'Skills', 'Target', 'Contact']
+});

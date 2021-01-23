62d9f46a8ca82524e8d6cd6303f6de4e159a83e2
dolenguyetthanh <dolenguyetthanh@gmail.com>
2021-01-23 23:58:29 +0700

    Complete home

--- js/main.js
+++ js/main.js
@@ -4,5 +4,33 @@ new fullpage('#fullpage', {
 	scrollHorizontally: true,
 	navigation: true,
 	anchors: ['home', 'aboutme', 'skills', 'target', 'contact'],
-	navigationTooltips: ['Home', 'About Me', 'Skills', 'Target', 'Contact']
+	navigationTooltips: ['Home', 'About Me', 'Skills', 'Target', 'Contact'],
+	navigationPosition: "left",
+	menu: "#menu"
 });
+$('#fullpage').fullpage({
+    sectionSelector: '.vertical-scrolling',
+    slideSelector: '.horizontal-scrolling',
+    controlArrows: false
+    // more options here
+});
+/*some var*/
+var click = 0;
+/*menu*/
+	$(".menu-icon").click(function() {
+		$(".hide").show();
+		if(click % 2 === 0) {
+			$(".menu-icon").css({
+			"transform": "rotate(180deg)",
+			"color": "#edff01"
+			});
+		}
+		else {
+			$(".menu-icon").css({
+			"transform": "rotate(0deg)",
+			"color": "#fff"
+			});
+		}
+		click++;
+	});
+
